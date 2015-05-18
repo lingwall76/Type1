c39131a76ab3b572539ec6895277009a91e85842
Justin Oakes <justin@justinoak.es>
2015-05-18 16:12:31 -0600

    Initial Commit

---
+++ Type1/AppDelegate.h
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.h
+//  Type1
+//
+//  Created by Justin Oakes on 5/18/15.
+//  Copyright (c) 2015 Devmtn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+
+@end
+
---
+++ Type1/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Type1
+//
+//  Created by Justin Oakes on 5/18/15.
+//  Copyright (c) 2015 Devmtn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
